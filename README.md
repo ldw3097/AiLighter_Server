86af035033d97c291b220e362d4fd542d0ab63d6
TUF\ldw <ldw3097@gmail.com>
2023-09-15 16:31:44 +0900

    init

--- README.md
+++ README.md
@@ -1,57 +1,40 @@
-# BertSum
+# KorBertSum 원본 데이터에서 학습까지
 
-**This code is from paper `Fine-tune BERT for Extractive Summarization`**(https://arxiv.org/pdf/1903.10318.pdf)
+이 코드는 기존 Korbertsum 코드에 aihub 자연어 데이터를 POS태깅하는 부분을 추가했습니다. 
 
-**!New: Please see our [full paper](https://arxiv.org/abs/1908.08345) with trained models**
+참고
+Korbertsum : https://velog.io/@raqoon886/KorBertSum-SummaryBot
+POS태깅 관련 : https://velog.io/@shoveling42/KorBertSum
 
-Package Requirements: pytorch pytorch_pretrained_bert tensorboardX multiprocess pyrouge
+## 실행 환경
+* Local
+* WSL Ubuntu
+* CUDA for WSL
 
-Some codes are borrowed from ONMT(https://github.com/OpenNMT/OpenNMT-py)
+* pyrouge
+* pytorch_pretrained_bert
+* tensorboardX
 
-## 한국어 문서 추출요약을 위한 Modified BertSum
+## 실행 방법
 
-### 해당 문서의 원저작권은 Nlpyang의 [BertSum](https://github.com/nlpyang/BertSum) 에 있습니다.
+### 1. 데이터 다운받기
+aihub 문서요약 텍스트를 다운받습니다.
+https://www.aihub.or.kr/aihubdata/data/view.do?currMenu=115&topMenu=100&aihubDataSe=realm&dataSetSn=97
 
-### **[저의 벨로그](https://velog.io/@raqoon886/KorBertSum-SummaryBot)** 에 코드와 플로우 설명이 있습니다.
+### 2. 데이터에 POS 태깅을 한다.
+학습시킬 json 파일을 raw_data 폴더에 넣고 src 폴더에서 아래 명령어를 실행합니다.
 
+    python article2json.py {파일 이름}
 
-#### Step 1. Prepare Data
+kiwi 형태소 분석이 오래걸릴수 있기 때문에 50개 단위로 테스트할것을 추천합니다.
 
-To run etri-api-scraper.py
-```
-python etri_api_scraper.py --input '.../train.jsonl' --api_key 'openapi key' --first_index 0 --last_index 5000
-```
+### 3. POS 태깅된 데이터를 임베딩한다.
 
-From newsdata to json
-```
-python article_to_json.py -mode train -news_dir '' -output ''
-```
+    python preprocess.py -mode format_to_bert -raw_path ../pos_data -save_path ../bert_data -vocab_file_path "{etri bert config path}"
 
-From json to Bert pytorch file
-```
-python preprocess.py -mode format_to_bert -raw_path ../json_data -save_path ../bert_data -vocab_file_path 'etri vocab file list'
-```
+etri bert config path 는 vocab.korean_morp.list 파일의 경로입니다.
+위 코드를 실행하면 bert_data 폴더 밑에 새로운 .pt파일이 생성됩니다.
 
-#### Step 2. Train
-
-To train with endoer-classifier
-```
-python train.py -mode train -encoder classifier -dropout 0.1 -bert_data_path ../bert_data/korean -model_path ../models/bert_classifier -lr 2e-3 -visible_gpus 0 -gpu_ranks 0 -world_size 1 -report_every 50 -save_checkpoint_steps 1000 -batch_size 1000 -decay_method noam -train_steps 1000 -accum_count 1 -log_file ../logs/bert_classifier -use_interval true -warmup_steps 8000 -bert_model ../001_bert_morp_pytorch -bert_config_path ../001_bert_morp_pytorch/bert_config.json -temp_dir ../temp
-```
-
-#### Step 3. Validate
-
-위의 코드에서 -mode validate로 수정하면 됩니다.
-
-#### Step 4. Test
-
-위의 코드에서 -mode test -test_from bert_model_path 로 수정하면 됩니다.
-
-실행 예시 파일 Newsdata_extractive_summ.ipynb 에 실행 예시가 있습니다.
-
-
-## CHATBOT
-
-Newsdata_summarybot.ipynb 에 실행 예시가 있습니다.
-![example](https://github.com/raqoon886/KoBertSum/tree/master/summbot.png)
-
+### 4. 코드를 학습시킨다.
+Newsdata_extractive_summ.ipynb를 실행시키면서 코드를 학습시킵니다.
+경로는 본인에 맞게 변경해주셔야 됩니다.
---
+++ src/article2json.py
@@ -0,0 +1,114 @@
+import json
+from kiwipiepy import Kiwi
+import sys
+
+def get_src(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file, strict=False)
+    
+        src_list = list()
+        num_of_arts = len(data["documents"])
+        # create src_list
+        for i in range(num_of_arts):
+            article = data["documents"][i]["text"]
+            
+            for sentence in article:
+                if len(sentence) == 0:
+                    del article[article.index(sentence)]
+
+            # analyze morphs via Kiwi tokenizer
+            
+            temp_list2 = list()
+            for j in range(2, len(article)):
+                print(article[j])
+                
+                for idx in range(len(article[j])):
+                    kiwi = Kiwi(num_workers=12)
+                    temp_list1 = list()
+                    tokenize_list = kiwi.tokenize(article[j][idx]["sentence"])[:-1]
+
+                    for z in range(len(tokenize_list)):
+                        token = tokenize_list[z].form + '/' + tokenize_list[z].tag
+                        temp_list1.append(token)
+                    temp_list2.append(temp_list1)
+            src_list.append(temp_list2)
+
+        with open('src.json', 'w', encoding='utf-8') as file:
+            json.dump(src_list, file, ensure_ascii=False)
+
+        return src_list
+    
+def get_extra_indices(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file, strict=False)
+        num_of_arts = len(data["documents"])
+
+        extra_indices_set = list()
+        for i in range(num_of_arts):
+            extractive = data["documents"][i]["extractive"]
+            extra_indices_set.append(extractive)
+        
+        print(extra_indices_set)
+        print("extraction completed")
+
+        return extra_indices_set
+    
+def get_tgt(file_path, src_list: list, extra_indices_set: list):
+    # print('source:', src_list)
+    # print(type(src_list))
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file, strict=False)
+    
+        num_of_arts = len(data["documents"])
+        # create tgt_list
+        tgt_list = list()
+        for i in range(num_of_arts):
+            article = data["documents"][i]["text"]
+            # add extractive's morphs
+            empty_list = list()
+            #print('article:', src_list[i])
+            for idx in extra_indices_set[0]:
+                print('idx:', idx)
+                
+                empty_list.append(src_list[i][idx-2])
+                #print('target list: ', empty_list)
+            tgt_list.append(empty_list)
+            # update extra_indices_set
+            del extra_indices_set[0]
+
+            if len(extra_indices_set) == 0:
+                break
+        print("tgt completed")
+
+        return tgt_list
+
+def make_json(file_path, src_list, tgt_list, output_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file, strict=False)
+        num_of_arts = len(data["documents"])
+
+        list_dic = list()
+        for i in range(num_of_arts):
+            mydict = {}
+            mydict['src'] = src_list[i]
+            mydict['tgt'] = tgt_list[i]
+            list_dic.append(mydict)
+
+        output_path = output_path + "korean.train.0.json"
+
+        with open(output_path, 'w', encoding='utf-8') as f:
+            json.dump(list_dic, f, ensure_ascii=False)
+
+def main():
+    file_name = sys.argv[1]
+    # 인풋 파일
+    input_path = f'../raw_data/{file_name}'
+    # 아웃풋 파일
+    output_path = f"../pos_data/"
+    src_txt = get_src(input_path)
+    extra_indices_set = get_extra_indices(input_path)
+    tgt_txt = get_tgt(input_path, src_txt, extra_indices_set)
+    make_json(input_path, src_txt, tgt_txt, output_path)
+
+if __name__ == '__main__':
+    main()
